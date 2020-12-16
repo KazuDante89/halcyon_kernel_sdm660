de4231af937a4a9c4c77a73fc4ff82d19c4c58d7
KazuDante89 <karlemmanueldorvil@mail.com>
2020-12-16 12:25:03 -0500

    Disable LTO_CLANG & THINLTO 	modified:   .circleci/build.sh 	modified:   arch/arm64/configs/lavender_defconfig

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -61,7 +61,7 @@ function compile() {
 # Zipping
 function zipping() {
     cd AnyKernel || exit 1
-    zip -r9 [EAS]-Nebula-Kernel_v0.0.3.zip *
+    zip -r9 [EAS]-Nebula-Kernel_v0.0.4.zip *
     cd ..
 }
 sendinfo
