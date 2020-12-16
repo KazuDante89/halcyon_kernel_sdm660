4adc4f3878f2d135e22bf1b1c993926de6f5463a
KazuDante89 <karlemmanueldorvil@mail.com>
2020-12-16 12:13:56 -0500

    disable vdso 	modified:   .circleci/build.sh 	modified:   arch/arm64/configs/lavender_defconfig

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -61,7 +61,7 @@ function compile() {
 # Zipping
 function zipping() {
     cd AnyKernel || exit 1
-    zip -r9 [EAS]-Nebula-Kernel_v0.0.2.zip *
+    zip -r9 [EAS]-Nebula-Kernel_v0.0.3.zip *
     cd ..
 }
 sendinfo
